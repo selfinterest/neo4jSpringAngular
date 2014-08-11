f247b0fbede5a4025f62bab244a16865614ea6a1
Terrence Watson <terrence.watson@contractor.cengage.com>
2014-08-11 13:39:51 -0400

    Spring template now works.

--- src/main/java/com/terrencewatson/RawController.java
+++ src/main/java/com/terrencewatson/RawController.java
@@ -7,6 +7,7 @@ import org.neo4j.cypher.javacompat.ExecutionResult;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.impl.util.StringLogger;
+
 import org.neo4j.rest.graphdb.query.RestCypherQueryEngine;
 import org.neo4j.rest.graphdb.util.QueryResult;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,11 @@ import org.springframework.context.annotation.ComponentScan;
 
 //import org.springframework.data.neo4j.annotation.QueryResult;
 import org.neo4j.rest.graphdb.util.QueryResult;
+import org.springframework.data.neo4j.conversion.Result;
 import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,18 +40,23 @@ public class RawController {
     @Autowired
     private SpringRestGraphDatabase graphDatabaseService;
 
+    @Autowired
+    private PlatformTransactionManager neo4jTransactionManager;
+
+    private Neo4jTemplate neo4jTemplate(){
+        return new Neo4jTemplate(graphDatabaseService, neo4jTransactionManager);
+    }
+
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
     @Transactional
     public String post(@RequestBody String query){
-
-        QueryResult<Map<String, Object>> result;
-        RestCypherQueryEngine engine = new RestCypherQueryEngine(graphDatabaseService.getRestAPI());  //As per @link http://stackoverflow.com/questions/21132234/null-exception-error-when-executing-cypher-query-from-java-code-neo4j
-        QueryBody queryBody = QueryBody.getQueryBodyFromString(query);
+        Neo4jTemplate template = neo4jTemplate();
+        Result<Map<String, Object>> result;
         Map<String, Object> params = null;
-
-        result = engine.query(queryBody.getQuery(), params);
+        QueryBody queryBody = QueryBody.getQueryBodyFromString(query);
+        result = template.query(queryBody.getQuery(), params);
 
         String rows = "";
         for(Map<String, Object> row: result){
@@ -57,7 +66,6 @@ public class RawController {
             rows += "\n";
         }
         return rows;
-
 
     }
 
--- src/main/java/com/terrencewatson/config/Neo4jStandalone.java
+++ src/main/java/com/terrencewatson/config/Neo4jStandalone.java
@@ -1,13 +1,16 @@
 package com.terrencewatson.config;
 
 import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.management.TransactionManager;
 import org.neo4j.rest.graphdb.RestGraphDatabase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.neo4j.config.EnableNeo4jRepositories;
 import org.springframework.data.neo4j.config.Neo4jConfiguration;
 import org.springframework.data.neo4j.rest.SpringRestGraphDatabase;
+import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -19,10 +22,57 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableNeo4jRepositories(basePackages = "org.springframework.data.neo4j.repository")
 @Import(AppConfig.class)
 @EnableTransactionManagement
+@Component
 public class Neo4jStandalone extends Neo4jConfiguration {
+
+    //@Autowired
+    //TransactionManager transactionManager;
 
     @Bean
     public SpringRestGraphDatabase graphDatabaseService(){
         return new SpringRestGraphDatabase("http://localhost:7474/db/data");
+    }
+
+
+
+    /*@Bean
+    public TransactionManager transactionManager(){
+        return new TransactionManager() {
+            @Override
+            public int getNumberOfOpenTransactions() {
+                return 0;
+            }
+
+            @Override
+            public int getPeakNumberOfConcurrentTransactions() {
+                return 0;
+            }
+
+            @Override
+            public int getNumberOfOpenedTransactions() {
+                return 0;
+            }
+
+            @Override
+            public long getNumberOfCommittedTransactions() {
+                return 0;
+            }
+
+            @Override
+            public long getNumberOfRolledBackTransactions() {
+                return 0;
+            }
+
+            @Override
+            public long getLastCommittedTxId() {
+                return 0;
+            }
+        }
+    }*/
+
+
+    @Bean
+    public Neo4jTemplate neo4jTemplate(SpringRestGraphDatabase graphDatabaseService) throws Exception {
+        return new Neo4jTemplate(graphDatabaseService, neo4jTransactionManager());
     }
 }
